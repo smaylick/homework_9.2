c25f63d90bf636b450b0feef05f44ce4572792d9
Sereja <serezagavrilenko@gmail.com>
2023-11-09 09:31:39 +0300

    Правки: 1. В файле requirements.txt указал используемые библиотеки с их точными версиями 2. Сделал краткий, читабельный и осмысленный локатор

--- test_google.py
+++ test_google.py
@@ -9,6 +9,5 @@ def screen_resolution():
 def test_search_google(screen_resolution): # использую фикстуру в которой задаю разрешение браузеру
     browser.open('https://www.google.ru/?hl=ru')
     browser.element('#APjFqb').click().type('hfisufhwueghwiouuehgwwegwegwageawgwg').press_enter()
-    browser.element('//*[@id="botstuff"]/div/div[2]/div/ul/li[1]').should(have.exact_text('Убедитесь, что все слова написаны без ошибок.'))
-
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
 
